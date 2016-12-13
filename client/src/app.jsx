9563dc3c1f6295516d1ce275e735996d3f081439
Viktors Jenovs <jviktors@gmail.com>
2016-12-13 19:56:16 +0200

    Inital commit

---
+++ client/src/app.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      user: null
+    };
+    this.handleLogin = this.handleLogin.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
+  }
+
+  componentWillMount() {
+    axios.get('/success').then(res => {
+      this.setState({
+        user: res.data.user
+      })
+    });
+  }
+
+  handleLogin() {
+    axios.get('/auth/github').then().catch();
+    // location.href = '/auth/github'
+  }
+
+  handleLogout() {
+    this.setState({
+      user: null
+    });
+    axios.get('/logout').then().catch();
+  }
+
+  render() {
+    return(
+      <div>
+        {this.state.user ? <button onClick={this.handleLogout}>Logout</button> :
+          <button onClick={this.handleLogin}>Login</button>}
+        {this.state.user && <h1>Welcome @{this.state.user}</h1>}
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
---
+++ index.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const session = require('express-session');
+
+const app = express();
+
+const GithubStrategy = require('passport-github2').Strategy;
+
+const clientID = "ffd10470e3eb457e804c",
+const clientSecret = "10a0edf349ea3a70f82abc87bda76a3229ba1199",
+const callbackURL = "http://127.0.0.1:3000/auth/github/callback"
+
+if (process.env.NODE_ENV === 'production') {
+  const clientID = "ffd10470e3eb457e804c",
+  const clientSecret = "10a0edf349ea3a70f82abc87bda76a3229ba1199",
+  const callbackURL = "http://127.0.0.1:3000/auth/github/callback"
+}
+
+passport.use(new GithubStrategy({
+    clientID: clientID,
+    clientSecret: clientSecret,
+    callbackURL: callbackURL
+  },
+  function(accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+  }
+));
+
+const ensureAuthenticated = (req, res, next) => {
+  if (!req.isAuthenticated()) return res.status(401).redirect('/');
+  next();
+}
+
+app.use(session({
+  secret: 'dfkldsjfio84395dsjf9eru',
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+app.use(express.static('./server/static'));
+app.use(express.static('./client/dist'));
+
+// this will start the GitHub Login process
+app.get('/auth/github', passport.authenticate('github'));
+
+// GitHub will call this URL
+app.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/')
+  }
+);
+
+app.get('/success', ensureAuthenticated, (req, res) => {
+  res.send({user: req.user.username});
+});
+
+app.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}...`);
+});
